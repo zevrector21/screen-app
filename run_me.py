c0db42a71722a38929924170a85d8bf8d3b2e4ca
zevrector21 <zevrector21@gmail.com>
2024-03-16 19:14:38 +0200

    update

--- run_me.py
+++ run_me.py
@@ -9,18 +9,20 @@ import schedule
 import time
 import json
 import pdb
-
+from transformers import pipeline
+from PIL import Image
 
 class Main:
-    directory_path = "files"
+    directory_path = "files/samples"
     screen_analysis_table = "image_analysis"
     screen_report_table = "image_report"
     file_extension = ".png"
-    threshold = 0.0048
+    nsfw_threshold = 0.0048
+    film_threshold = 0.75
 
     def __init__(self):
         self.model = predict.load_model("./nsfw_mobilenet2.224x224.h5")
-        pass
+        self.film_pipe = pipeline("image-classification", model="pszemraj/beit-large-patch16-512-film-shot-classifier")
 
     def test_model(self):
         class_names = {0: "skype", 1: "skack", 2: "vs_code"}
@@ -50,13 +52,19 @@ class Main:
 
         screen_list = self.fetch_screens()
         file_list = os.listdir(self.directory_path)
-        for screen in screen_list:
+        for screen in screen_list[::-1]:
             # screen[1] file path
             filename = random.choice(file_list)
             if not filename.endswith(self.file_extension):
                 continue
-            result = predict.classify(self.model, f'{self.directory_path}/{filename}')
-            self.insert_data_into_db(screen, result)
+
+            full_path_name = f'{self.directory_path}/{filename}'
+            image = Image.open(full_path_name)
+
+            nsfw_result = predict.classify(self.model, full_path_name)
+            film_result = self.film_pipe(image)[0]
+            
+            self.insert_data_into_db(screen, nsfw_result, film_result)
 
     def fetch_screens(self):
         latest_screen_id = 0
@@ -75,7 +83,7 @@ class Main:
         try:
             print("Loading new screens from screens table...")
             sql = f'''
-                SELECT id, client_id, laptop_id, location FROM screens WHERE id > {latest_screen_id} ORDER BY created_at DESC
+                SELECT id, client_id, laptop_id, location FROM screens WHERE id > {latest_screen_id} ORDER BY created_at DESC LIMIT 10
             '''
             self.cursor.execute(sql)
             result = self.cursor.fetchall()
@@ -85,7 +93,7 @@ class Main:
         
         return result
 
-    def insert_data_into_db(self, screen, result):
+    def insert_data_into_db(self, screen, nsfw_result, film_result={}):
         try:
             output_sql = f'''
                 INSERT INTO {self.screen_analysis_table} (
@@ -95,18 +103,26 @@ class Main:
                     hentai,
                     sexy,
                     porn,
+                    film, 
                     status
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             '''
             output_records = []
-            report_records = []
-            for key, value in result.items():
+            report_records = []           
+            film_score = film_result['score']
+            if film_result['label'] not in ['extremeLongShot', 'fullShot', 'longShot', 'mediumCloseUp', 'mediumShot']:
+                film_score = 0
+
+            for key, value in nsfw_result.items():
                 report_value = {}
                 output_status = "safe"
 
                 for v_key, v_value in value.items():
-                    if v_value > self.threshold and v_key in ['hentai', 'sexy', 'porn']:
+                    if v_value > self.nsfw_threshold and v_key in ['hentai', 'sexy', 'porn']:
                         report_value[v_key] = v_value
+
+                    if film_score > self.film_threshold:
+                        report_value['film'] = film_score
 
                 if report_value != {}:
                     output_status = "unsafe"
@@ -124,6 +140,7 @@ class Main:
                     value['hentai'], 
                     value['sexy'],
                     value['porn'],
+                    film_score,
                     output_status
                 ))
 
@@ -172,6 +189,7 @@ class Main:
                     hentai FLOAT,
                     sexy FLOAT,
                     porn FLOAT,
+                    film FLOAT,
                     status VARCHAR(30),
                     created_at TIMESTAMP default current_timestamp
                 )
